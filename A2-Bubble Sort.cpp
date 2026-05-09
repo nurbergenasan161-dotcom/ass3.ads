a12e34f24974c282fc4640320a40062a9c569531
nurbergenasan161-dotcom <nurbergenasan161@gmail.com>
2026-05-09 21:33:53 +0500

    Implement Bubble Sort algorithm in A2-Bubble Sort.cpp

---
+++ A2-Bubble Sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
